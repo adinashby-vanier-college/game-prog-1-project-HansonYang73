634ae80a8ffafd0fe035a6a9e0db4d676a61b40b
Hanson Yang <hansonyang73@gmail.com>
2024-11-19 20:56:00 -0500

    Added legendary sword gif

--- legendOfTheFallen/LegendarySword.java
+++ legendOfTheFallen/LegendarySword.java
@@ -7,7 +7,12 @@ import greenfoot.*;
  */
 public class LegendarySword extends Artifact
 {
-
+    private GifImage knightGif =  new  GifImage("LegendarySword.gif");
+    
+    public void act(){
+        setImage(knightGif.getCurrentImage());
+    }
+    
     public void getArtifact(){
         scaleImage();
         Settings.baseAtk = 35;
