ac60357ff014fe3f27a874ad6cde2a3c7bacbbc9
Oliver D'Avino <6252915@edu.vaniercollege.qc.ca>
2024-11-28 14:16:10 -0500

    test

---
+++ legendOfTheFallen/World3.java
@@ -0,0 +1,18 @@
+// WARNING: This file is auto-generated and any changes to it will be overwritten
+import lang.stride.*;
+import java.util.*;
+import greenfoot.*;
+
+/**
+ * 
+ */
+public class World3 extends TemplateWorld
+{
+
+    /**
+     * Constructor for objects of class World3.
+     */
+    public World3()
+    {
+    }
+}
